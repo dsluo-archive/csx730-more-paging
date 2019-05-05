c7fce20952e1d4cb5ad09070cd769a68966f7b7e
dsluo <mail@dsluo.me>
2019-05-05 01:36:08 -0400

    submission of more-paging

--- paging.c
+++ paging.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct addr_t{
+struct addr_t {
     unsigned long pn0:13;
     unsigned long pn1:13;
     unsigned long pn2:13;
     unsigned long pn3:13;
     unsigned long off:12;
-
 };
 
 typedef struct addr_t addr_t;
 
-union entry_type {
+union entry_t {
     addr_t frame;
-    union entry_type * next;
+    union entry_t * next;
 };
 
-typedef union entry_type entry_type;
+typedef union entry_t entry_t;
 
+#define COUNT (1ul << 13)
+
+entry_t table0[COUNT];
+
+addr_t virt_to_phys(addr_t virt) {
+    entry_t entry = table0[virt.pn0];
+
+    if (entry.next == null) {
+        entry.next = malloc(sizeof(entry_t) * COUNT);
+        memset(entry.next, 0, sizeof(table0));
+    }
+
+}
 
 int main() {
 
-  setvbuf(stdout, NULL, _IONBF, 0); // disable output buffering
-  printf("addr_t size: %lu\n", sizeof(addr_t));
+    setvbuf(stdout, NULL, _IONBF, 0); // disable output buffering
+    printf("addr_t size: %lu\n", sizeof(addr_t));
 
-  addr_t addr = { 1, 2, 3, 4, 10 };
+    addr_t addr = { 1, 3, 7, 15, 31 };
 
-  return 0;
+    memset(&table0, 0, sizeof(table0));
+
+    return 0;
 
 } // main
